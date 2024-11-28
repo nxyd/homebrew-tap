7677475c7fbc3be39cd5bc51e51c0d412613b868
Shawn Xiao <xiaoyongdong@outlook.com>
2024-11-28 12:06:46 +0800

    add ClashX

---
+++ Casks/clashx-meta.rb
@@ -0,0 +1,31 @@
+cask "clashx-meta" do
+    version "1.4.5"
+    sha256 "c7680b24e075e7ad885d23700d50413fd03e02fdf229fae125c3dd803edd3c0d"
+  
+    url "https://github.com/MetaCubeX/ClashX.Meta/releases/download/v#{version}/ClashX.Meta.zip"
+    name "ClashX Meta"
+    desc "Rule-based custom proxy with GUI based on Clash.Meta"
+    homepage "https://github.com/MetaCubeX/ClashX.Meta"
+  
+    auto_updates true
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+  
+    app "ClashX Meta.app"
+  
+    uninstall launchctl: "com.metacubex.ClashX.ProxyConfigHelper",
+              quit:      "com.metacubex.ClashX",
+              delete:    [
+                "/Library/LaunchDaemons/com.metacubex.ClashX.ProxyConfigHelper.plist",
+                "/Library/PrivilegedHelperTools/com.metacubex.ClashX.ProxyConfigHelper",
+              ]
+  
+    zap trash: [
+      "~/Library/Application Support/com.metacubex.ClashX.meta",
+      "~/Library/Caches/com.metacubex.ClashX.meta",
+      "~/Library/Logs/ClashX Meta",
+      "~/Library/Preferences/com.metacubex.ClashX.meta.plist",
+    ]
+  end
